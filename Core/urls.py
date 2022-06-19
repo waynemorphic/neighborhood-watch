ddf7deb9781a1e7bfff4a284664195929f2ff17d
waynemorphic <wayneklyde@gmail.com>
2022-06-19 18:25:21 +0300

    view functions urls

--- Core/urls.py
+++ Core/urls.py
@@ -3,5 +3,12 @@ from Core import views
 
 urlpatterns = [
     path('', views.home, name = 'home/'),
+    path('home/new_post', views.new_post, name = 'add post'),
+    path('home/profile', views.profile, name = 'user profile'),
+    path('home/police', views.police, name = 'police'),
+    path('home/businesses', views.business, name = 'business'),
+    path('home/health_services', views.health, name = 'health'),
+    path('home/search_results', views.search_results, name = 'search results'),
+    path('logout', views.sign_out, name = 'logout'),
    
 ]
